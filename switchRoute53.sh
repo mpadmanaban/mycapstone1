c70ed54b6c9abe6661362e0a3574e04213b96b30
mpadmanaban <mpadmanaban@gmail.com>
2020-05-12 22:27:58 -0700

    Add delay in swith route script to get external dns name

--- switchRoute53.sh
+++ switchRoute53.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #script to change Route53 name
 #Pass in the ExternalDNS Alias name to switch the Route53 alias to
 serviceName=$1
@@ -8,7 +8,16 @@ hostedZoneID="Z3AADJGX6KTTL2"
 echo Service :
 kubectl get service $serviceName
 echo .
-lbHostname=$(kubectl get service $serviceName -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+lbHostname=""
+retry=30
+while [ "$lbHostname" == "" ]; do
+  lbHostname=$(kubectl get service $serviceName -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+  if [ "$lbHostname" == "" ]; then sleep 1 ; else break; ;fi
+  let retry=$retry-1
+  if [ "$retry" == "0" ]; then break; fi
+  
+done
+
 echo $lbHostname , $hostedZoneID
 inputJson="{\"Changes\": [{\"Action\": \"UPSERT\",\"ResourceRecordSet\": {\"Name\": \"mp-capstone.com\", \
     \"Type\": \"A\",\"AliasTarget\":{\"HostedZoneId\": \"$hostedZoneID\", \
